9a04f222b342e58cb7625531999744a0baac31ac
Khavkin <hdm@ukr.net>
2023-07-07 13:56:00 +0300

    added model category

---
+++ models/category-model.js
@@ -0,0 +1,10 @@
+const { Schema, model, ObjectId } = require('mongoose');
+
+const categorySchema = new Schema({
+  _id: ObjectId,
+  name: String,
+});
+
+const Category = model('category', categorySchema);
+
+module.export = Category;
