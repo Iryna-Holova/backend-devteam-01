e958f2e7960237f9d7b7a3fd3e8783a8e688a18a
Khavkin <hdm@ukr.net>
2023-07-09 13:49:03 +0300

    Change controllers logic

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -3,46 +3,62 @@ const { Recipe } = require('../models/recipe-model');
 
 //
 const getMainPage = async (req, res) => {
-  const { limit = 1 } = req.body;
+  const { limit = 1 } = req.query;
   const arrayOfMainPagePromises = [];
   arrayOfMainPagePromises.push(
-    Recipe.find({ category: 'Breakfast' }, null, { limit }).sort({ name: 1 }).exec()
+    //    Recipe.find({ category: 'Breakfast' }, null, { limit }).sort({ name: 1 }).exec()
+    Recipe.find({ category: 'Breakfast' }).sort({ _id: -1 }).limit(limit)
   );
   arrayOfMainPagePromises.push(
-    Recipe.find({ category: 'Miscellaneous' }, null, { limit }).sort({ name: 1 }).exec()
+    // Recipe.find({ category: 'Miscellaneous' }, null, { limit }).sort({ name: 1 }).exec()
+    Recipe.find({ category: 'Miscellaneous' }).sort({ _id: -1 }).limit(limit)
   );
   arrayOfMainPagePromises.push(
-    Recipe.find({ category: 'Chicken' }, null, { limit }).sort({ name: 1 }).exec()
+    //  Recipe.find({ category: 'Chicken' }, null, { limit }).sort({ name: 1 }).exec()
+    Recipe.find({ category: 'Chicken' }).sort({ _id: -1 }).limit(limit)
   );
   arrayOfMainPagePromises.push(
-    Recipe.find({ category: 'Desserts' }, null, { limit }).sort({ name: 1 }).exec()
+    //  Recipe.find({ category: 'Desserts' }, null, { limit }).sort({ name: 1 }).exec()
+    Recipe.find({ category: 'Dessert' }).sort({ _id: -1 }).limit(limit)
   );
 
-  Promise.all(arrayOfMainPagePromises).then(results => {
-    const initialValue = {
-      Breakfast: [],
-      Miscellaneous: [],
-      Chicken: [],
-      Desserts: [],
-    };
+  const [Brekfast, Miscellaneous, Chicken, Desserts] = await Promise.allSettled(
+    arrayOfMainPagePromises
+  );
+  const result = {
+    Brekfast: Brekfast.value,
+    Miscellaneous: Miscellaneous.value,
+    Chicken: Chicken.value,
+    Desserts: Desserts.value,
+  };
 
-    const result = results
-      .flatMap(item => {
-        return [...item];
-      })
-      .reduce((queryResult, recipe) => {
-        queryResult[recipe.category].push(recipe);
+  // Promise.all(arrayOfMainPagePromises).then(results => {
+  //   const initialValue = {
+  //     Breakfast: [],
+  //     Miscellaneous: [],
+  //     Chicken: [],
+  //     Desserts: [],
+  //   };
 
-        return queryResult;
-      }, initialValue);
+  //   const result = results
+  //     .flatMap(item => {
+  //       return [...item];
+  //     })
+  //     .reduce((queryResult, recipe) => {
+  //       queryResult[recipe.category].push(recipe);
 
-    res.json(result);
-  });
+  //       return queryResult;
+  //     }, initialValue);
+
+  //   res.json(result);
+  // });
+  res.json(result);
 };
 
 const getRecipesByCategory = async (req, res) => {
   const { category } = req.params;
-  const { limit = 8 } = req.body;
+  const { limit = 8 } = req.query;
+  console.log(category);
 
   const response = await Recipe.find({ category }).sort({ _id: -1 }).limit(limit);
   //.populate('ingredients.id', 'name desc img');  uncomment if need or delete
--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,29 +1,21 @@
-const express = require("express");
+const express = require('express');
 
-const ctrl = require("../../controllers/categories-controller");
-const ctrlRecipes = require("../../controllers/recipes-controller");
-const {
-  validateBody,
-  validateParams,
-  validateId,
-} = require("../../middlewares");
-const { schemas } = require("../../models/recipe-model");
+const ctrl = require('../../controllers/categories-controller');
+const ctrlRecipes = require('../../controllers/recipes-controller');
+const { validateParams, validateId, validateQuery } = require('../../middlewares');
+const { schemas } = require('../../models/recipe-model');
 
 const router = express.Router();
 
-router.get("/category-list", ctrl.getAllCategories);
+router.get('/category-list', ctrl.getAllCategories);
+router.get('/main-page', validateQuery(schemas.limitMainPageSchema), ctrlRecipes.getMainPage);
 router.get(
-  "/main-page",
-  validateBody(schemas.limitMainPageSchema),
-  ctrlRecipes.getMainPage
-);
-router.get(
-  "/category/:category",
+  '/category/:category',
   validateParams(schemas.getByCategoryParamsSchema),
-  validateBody(schemas.getByCategoryBodySchema),
+  validateQuery(schemas.getByCategoryBodySchema),
   ctrlRecipes.getRecipesByCategory
 );
 
-router.get("/:id", validateId, ctrlRecipes.getRecipeById);
+router.get('/:id', validateId, ctrlRecipes.getRecipeById);
 
 module.exports = router;
