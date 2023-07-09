97b456f03e342333eeea210d80479f17e96ca9c8
Volodymyr Yarchuk <volodia.iarcguk.forwork@gmail.com>
2023-07-09 12:52:23 +0300

    FIXED: recipe model ingredients id

--- models/recipe-model.js
+++ models/recipe-model.js
@@ -45,8 +45,7 @@ const recipeSchema = new Schema(
     ingredients: {
       type: [
         {
-          _id: false,
-          id: {
+          _id: {
             type: String,
             required: [true, "Ingredient is required"],
             ref: "ingredient",
@@ -79,7 +78,7 @@ const createOwnRecipeSchema = Joi.object({
   ingredients: Joi.array()
     .items(
       Joi.object({
-        id: Joi.string().hex().length(24).required(),
+        _id: Joi.string().hex().length(24).required(),
         measure: Joi.string().required(),
       })
     )
