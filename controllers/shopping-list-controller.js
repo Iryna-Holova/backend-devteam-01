e972d72469fc123d3831f9ecf6984a0f9f1e5e6a
Khavkin <hdm@ukr.net>
2023-08-16 23:43:21 +0300

    ADD: clear and add ingredients in recipe

--- controllers/shopping-list-controller.js
+++ controllers/shopping-list-controller.js
@@ -79,10 +79,7 @@ async function create(req, res) {
   });
 
   if (isRecipeMeasureDoubled) {
-    throw HttpError(
-      400,
-      "The ingredient in a user's list from the recipe is already added"
-    );
+    throw HttpError(400, "The ingredient in a user's list from the recipe is already added");
   }
 
   await User.findOneAndUpdate(
@@ -145,10 +142,7 @@ async function deleteById(req, res) {
   );
 
   if (!updatedUser) {
-    throw HttpError(
-      404,
-      "The ingredient is not found with such a measure in the user's list"
-    );
+    throw HttpError(404, "The ingredient is not found with such a measure in the user's list");
   }
 
   await User.findOneAndUpdate(
@@ -171,8 +165,80 @@ async function deleteById(req, res) {
   res.json({ message: "The ingredient deleted" });
 }
 
+async function addAllInRecipe(req, res) {
+  const { _id: userId } = req.user;
+  const { id: recipeId } = req.params;
+
+  const { ingredients } = await Recipe.findById(recipeId, "ingredients");
+  const { shoppingList } = await User.findById(userId);
+
+  console.log(ingredients);
+
+  // Проверяем наличие ингредиентов в shoppingList в списке ингредиентов рецепта. Если есть, проверяем наличие рецепта в measures.
+  // Если нет, добавляем и удаляем из списка ингредиентов.
+  const shoppingListNew = shoppingList.reduce((shoppingList, current) => {
+    const index = ingredients.findIndex(
+      ingredient => ingredient.id === current.ingredientId.toString()
+    );
+    console.log(index);
+    if (index < 0) return [...shoppingList, current];
+    else {
+      console.log(recipeId, current.measures);
+      const recipeIndex = current.measures.findIndex(
+        measure => measure.recipeId.toString() === recipeId
+      );
+      if (recipeIndex < 0) {
+        current.measures.push({ recipeId, measure: ingredients[index].measure });
+      }
+      ingredients.splice(index, 1);
+      return [...shoppingList, current];
+    }
+  }, []);
+
+  //Добавляем оставшиеся ингредиенты в shoppingList
+  if (ingredients.length > 0)
+    ingredients.forEach(ingredient => {
+      shoppingListNew.push({
+        ingredientId: ingredient.id,
+        measures: [{ recipeId, measure: ingredient.measure }],
+      });
+    });
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { shoppingList: [...shoppingListNew] },
+    {
+      new: true,
+    }
+  );
+
+  res.json(shoppingListNew);
+}
+
+async function delAllInRecipe(req, res) {
+  console.log(req.params);
+  res.json(req.params);
+}
+
+async function clear(req, res) {
+  const { _id: userId } = req.user;
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { shoppingList: [] },
+    {
+      new: true,
+    }
+  );
+
+  res.json({ message: "Shopping list cleared" });
+}
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   create: ctrlWrapper(create),
   deleteById: ctrlWrapper(deleteById),
+  addAllInRecipe: ctrlWrapper(addAllInRecipe),
+  delAllInRecipe: ctrlWrapper(delAllInRecipe),
+  clear: ctrlWrapper(clear),
 };
